9ec7d7dce3af5ea01b8eab12db7b03dff078421a
pranalimahale <37696645+pranalimahale@users.noreply.github.com>
2020-10-12 12:07:57 +0530

    Add files via upload

---
+++ Controllers/ProductShowController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopBridge.ViewModel;
+using ShopBridge.Models;
+
+
+namespace ShopBridge.Controllers
+{
+    public class ProductShowController : Controller
+    {
+        ShopDetailsDataEntities1 db = new ShopDetailsDataEntities1();
+        // GET: /ProductShow/
+
+        public ActionResult Index(int Id)
+        {
+            tbl_shop ts = db.tbl_shop.Single(x => x.Id == Id);
+            return View(ts);
+        }
+
+    }
+}
